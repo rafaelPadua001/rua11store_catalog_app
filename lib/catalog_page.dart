5aedbdd0da2caaa72820d54d1c379cf75239fa35
Rafael Pádua <rafael.f.p.faria@hotmail.com>
2025-05-07 15:54:16 -0300

    Fix
    Remove subcategories on home page.

--- lib/catalog_page.dart
+++ lib/catalog_page.dart
@@ -18,6 +18,8 @@ class _CatalogPageState extends State<CatalogPage> {
   final TextEditingController _searchController = TextEditingController();
   String searchQuery = "";
   String selectedCategory = "";
+  final List<Product> products = [];
+  final List<Categories> categories = [];
 
   @override
   void initState() {
@@ -38,59 +40,6 @@ class _CatalogPageState extends State<CatalogPage> {
           ).fetchProducts(),
     );
   }
-
-  final List<Product> products = [
-    // Product(
-    //   name: "Produto 1",
-    //   description: "Descrição do Produto 1",
-    //   image: "assets/products/rollingPapers/61zuBpzampL.webp",
-    //   price: "R\$ 100,00",
-    //   phone: "+556195051731",
-    //   category: "Sedas",
-    // ),
-    // Product(
-    //   name: "Produto 2",
-    //   description: "Descrição do Produto 2",
-    //   image: "assets/products/rollingPapers/seda_zomo-miniblack-1.webp",
-    //   price: "R\$ 200,00",
-    //   phone: "+5561995051731",
-    //   category: "Sedas",
-    // ),
-    // Product(
-    //   name: "Produto 3",
-    //   description: "Descrição do Produto 2",
-    //   image: "assets/products/rollingPapers/5672.webp",
-    //   price: "R\$ 200,00",
-    //   phone: "+5561995051731",
-    //   category: "Tabacos",
-    // ),
-    // Product(
-    //   name: "Produto 4",
-    //   description: "Descrição do Produto 2",
-    //   image: "assets/products/rollingPapers/tabaco-la-revolucion-lrv-virginia-golden-30g-smoker-space-0m62hic7k3.webp",
-    //   price: "R\$ 200,00",
-    //   phone: "+5561995051731",
-    //   category: "Tabacos",
-    // ),
-    // Product(
-    //   name: "Produto 5",
-    //   description: "Descrição do Produto 2",
-    //   image: "assets/products/rollingPapers/c19178be-1ff2-47a7-927e-0cc5e005c750.webp",
-    //   price: "R\$ 200,00",
-    //   phone: "+5561995051731",
-    //   category: "Filtros",
-    // ),
-    // Product(
-    //   name: "Produto 6",
-    //   description: "Descrição do Produto 2",
-    //   image: "assets/products/rollingPapers/2_-_chillin_tips_22out1-min-1f5.webp",
-    //   price: "R\$ 200,00",
-    //   phone: "+5561995051731",
-    //   category: "Piteiras",
-    // ),
-  ];
-
-  final List<Categories> categories = [];
 
   @override
   Widget build(BuildContext context) {
@@ -138,11 +87,13 @@ class _CatalogPageState extends State<CatalogPage> {
                   padding: EdgeInsets.symmetric(horizontal: 10),
                   children:
                       controller.categories
+                          .where((category) => category.isSubcategory == false)
                           .map(
                             (category) => Padding(
                               padding: const EdgeInsets.symmetric(
                                 horizontal: 5,
                               ),
+
                               child: CategoriesChip(
                                 categories: category,
                                 onTap: () {
