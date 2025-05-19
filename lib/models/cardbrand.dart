c4ffbd9ec8f2457ada8d0627dd4a662e5f189305
Rafael Pádua <rafael.f.p.faria@hotmail.com>
2025-05-19 13:08:37 -0300

    Feat
    Create brand card dropDown

---
+++ lib/models/cardbrand.dart
@@ -0,0 +1,11 @@
+class CardBrand {
+  final String name;
+  final String paymentMethodId;
+  final String imageUrl;
+
+  CardBrand({
+    required this.name,
+    required this.paymentMethodId,
+    required this.imageUrl,
+  });
+}
