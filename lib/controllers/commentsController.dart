57123583f37320ef9a57d871f81d1fe1cda493c2
Rafael Pádua <rafael.f.p.faria@hotmail.com>
2025-07-20 00:11:33 -0300

    Feat
    create update and delete comment functions

--- lib/controllers/commentsController.dart
+++ lib/controllers/commentsController.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 class Commentscontroller {
   final baseUrl = dotenv.env['API_URL'];
 
-  Future<bool> saveComment({
+  Future<Map<String, dynamic>?> saveComment({
     required String comment,
     required String userId,
     required String userName,
@@ -29,13 +29,54 @@ class Commentscontroller {
       );
 
       if (response.statusCode == 200 || response.statusCode == 201) {
-        return true;
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        return data;
       } else {
         print('Erro ao salvar comentário: ${response.body}');
-        return false;
+        return null;
       }
     } catch (error) {
       print('Exceção ao salvar comentário: $error');
+      return null;
+    }
+  }
+
+  Future<Map<String, dynamic>?> updateComment({
+    required int commentId,
+    required String comment,
+  }) async {
+    final url = Uri.parse('$baseUrl/comments/update/${commentId}');
+    try {
+      final response = await http.put(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'comment': comment}),
+      );
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        return data;
+      } else {
+        print('Erro ao salvar commentário: ${response.body}');
+      }
+    } catch (error) {
+      print('Exceção ao salvar comentaro: $error');
+      return null;
+    }
+  }
+
+  Future<bool> deleteComment(int commentId) async {
+    final url = Uri.parse('$baseUrl/comments/delete/${commentId}');
+    try {
+      final response = await http.delete(url);
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        print('Falha ao deletar. Código: ${response.statusCode}');
+        return false;
+      }
+    } catch (e) {
+      print('Erro ao deletar comentário ${e}');
       return false;
     }
   }
