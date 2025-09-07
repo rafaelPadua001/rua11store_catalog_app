802d79f46979a8faf06ca9d6ddcf42f9b18ca83c
Rafael Pádua <145411939+rafaelPadua001@users.noreply.github.com>
2025-09-07 13:41:26 -0300

    Fix
    Fix web and mobile links

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Rafae\OneDrive\Documentos\GitHub\rua11store_catalog_app"
+export "FLUTTER_APPLICATION_PATH=C:\GitHub\rua11store_catalog_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/utils/web_utils.dart
+++ lib/utils/web_utils.dart
@@ -1,6 +1,7 @@
+import 'dart:html' as html;
 // Apenas para Web
 void clearUrl() {
   // Import 'dart:html' só será feito aqui
-  import 'dart:html' as html;
+
   html.window.history.pushState(null, 'Rua11Store', '/');
 }
